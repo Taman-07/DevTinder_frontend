8c83238617e3958179cbd3007b84e19c5eeb5d9f
Taman-07 <tamanjot707@gmail.com>
2026-04-14 20:23:10 +0530

    updating feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,59 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from 'react-redux';
+import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({ user }) => {
-    const { firstName, lastName, about, photoUrl, age, gender } = user;
-  return (
-    <div className="card w-full max-w-sm bg-base-300 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img
-      src={photoUrl}
-      alt="Photo"
-      className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">{ firstName + " " + lastName }</h2>
-    {age && gender && <p>{ age + " , " + gender }</p>}
-    <p>{ about }</p>
-    <div className="card-actions justify-center my-4">
-      <button className="btn btn-primary">Ignore</button>
-      <button className="btn btn-secondary">Interested</button>
-    </div>
-  </div>
-</div>
-  );
-}
+  const { _id, firstName, lastName, about, photoUrl, age, gender } = user;
 
-export default UserCard
+  const dispatch = useDispatch();
+
+  const handleSendRequest = async(status, _id) => {
+    try{
+      const res = await axios.post(BASE_URL + "/request/send/" + status + "/" + _id,
+        {},
+        {withCredentials: true},
+      );
+      dispatch(removeUserFromFeed(_id));
+    }
+    catch(err){
+      console.log(err.message);
+    }
+  };
+
+
+  return (
+    <div className="flex justify-center">
+      <div className="card w-80 bg-base-200 shadow-2xl rounded-2xl overflow-hidden hover:scale-105 transition duration-300">
+        <figure className="bg-black">
+          <img src={photoUrl} alt="user" className="w-full max-h-[400px] object-contain"/>
+        </figure>
+
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-xl font-bold">{firstName} {lastName}</h2>
+
+          {age && gender && (
+            <p className="text-sm opacity-70">
+              {age} • {gender}
+            </p>
+          )}
+
+          <p className="text-sm mt-2">{about}</p>
+
+          <div className="card-actions justify-center gap-4 mt-4">
+
+            <button className="btn btn-outline btn-error" 
+              onClick={() => handleSendRequest("ignored", _id)}>Ignore</button>
+
+            <button className="btn btn-outline btn-primary"
+              onClick={() => handleSendRequest("interested", _id)}>Interested</button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
